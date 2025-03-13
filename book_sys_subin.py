56725735999e6b10eaec752954bf4223279f0345
sangyeob <rlatkdduq1234@gmail.com>
2025-03-13 09:37:58 +0900

    수정 전

--- book_sys_subin.py
+++ book_sys_subin.py
@@ -123,7 +123,6 @@ while True:
         if not library:
             print("등록된 도서가 없습니다.")
         else:
-<<<<<<< HEAD
             for title, (author, category, available) in library.items():
                 status = "Available" if available else "Checked Out"
                 print(f"제목: '{title}', 저자: {author}, 카테고리: {category}, 상태: {status}")
@@ -136,11 +135,3 @@ while True:
         print("잘못된 선택입니다. 다시 입력하세요.")
 
 
-=======
-            print("\n==== 도서 목록 ====")
-            # 헤더 출력 (각 항목의 제목을 출력)
-            print(f"{'번호':<5} {'제목':<30} {'저자':<20} {'카테고리':<10} {'상태':<15}")
-
-            ## insta : soobimy
-  
->>>>>>> f4f4503b47d8018ce844f3bf1739a39d163bbdcf
